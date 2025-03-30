a2ff3a06d6869feeae1a781b038993eb4039ade7
Jaywinner <57271422+Jaywinner@users.noreply.github.com>
2025-03-30 14:13:15 +0100

    Add sqlidetect/config/globals.php 1743340394012

---
+++ sqlidetect/config/globals.php
@@ -0,0 +1,9 @@
+<?php
+// Global configuration will be set during installation
+$db_host = '';
+$db_user = '';
+$db_password = '';
+$db_name = 'sqlidetect';
+$db_type = 'MySQL';
+$install = false;
+?>
